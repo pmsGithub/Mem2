618b738cd90c507724cafb261fbbae62b6524f4b
PMS <homeaccts@sweazey.com>
2020-07-10 14:28:15 +0200

    Initial Commit

---
+++ Mem2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mem2
+//
+//  Created by Paul M Sweazey on 2020.07.10.
+//  Copyright © 2020 Paul M Sweazey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
